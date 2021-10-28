f385d434e7b99e2830800085b88c7a0e32463dfd
James Chacon <james.chacon@snowflake.com>
2021-10-28 14:01:35 -0700

    Cleanup from proto/package splits.
    Move generate line into a file scoped to the proto package which just contains that and nothing else.
    
    Remove this from the server comments.

--- services/exec/server/exec.go
+++ services/exec/server/exec.go
@@ -1,9 +1,5 @@
 package server
 
-// To regenerate the proto headers if the .proto changes, just run go generate
-// This comment encodes the necessary magic:
-//go:generate protoc --go_out=. --go_opt=paths=source_relative --go-grpc_out=require_unimplemented_servers=false:. --go-grpc_opt=paths=source_relative exec.proto
-
 import (
 	"bytes"
 	"context"
---
+++ services/localfile/localfile.go
@@ -0,0 +1,5 @@
+package localfile
+
+// To regenerate the proto headers if the .proto changes, just run go generate
+// and this encodes the necessary magic:
+//go:generate protoc --go_out=. --go_opt=paths=source_relative --go-grpc_out=require_unimplemented_servers=false:. --go-grpc_opt=paths=source_relative localfile.proto
