a2b3fba5e193bbefa2cd8c292bbb5ffcdea7827d
James Chacon <james.chacon@snowflake.com>
2021-11-01 16:25:20 -0700

    Move all command execution into a utility function.
    This can enforce invariants such as "must be an absolute and clean path"
    
    Move TrimString into util as well.
    
    Provide a testutil function to resolve a naked binary name (sh, false, cat) to a real path.
    
    Replace all test binary referecnes with lookups for absolute paths.

--- services/util/util.go
+++ services/util/util.go
@@ -12,19 +12,31 @@ import (
 )
 
 type CommandRun struct {
+<<<<<<< HEAD
 	Stdout   *bytes.Buffer
 	Stderr   *bytes.Buffer
 	Error    error
 	ExitCode int
+=======
+	Stdout    *bytes.Buffer
+	Stderr    *bytes.Buffer
+	WaitError error
+	ExitCode  int
+>>>>>>> Move all command execution into a utility function.
 }
 
 // RunCommand will take the given binary and args and execute it returning all
 // relevent state (stdout, stderr, errors, etc).
 //
 // The binary must be a clean absolute path or an error will result and nothing will
+<<<<<<< HEAD
 // be run. Any other errors (starting or from waiting) are recorded in the Error field.
 // Errors returned directly will be a status.Error and Error will be whatever the exec
 // library returns.
+=======
+// be run. An error will also be returned if the binary cannot start.
+// These errors will be a status.Error.
+>>>>>>> Move all command execution into a utility function.
 func RunCommand(ctx context.Context, bin string, args []string) (*CommandRun, error) {
 	if !filepath.IsAbs(bin) {
 		return nil, status.Errorf(codes.InvalidArgument, "%s is not an absolute path", bin)
@@ -43,11 +55,21 @@ func RunCommand(ctx context.Context, bin string, args []string) (*CommandRun, er
 	cmd.Stdout = run.Stdout
 	cmd.Stderr = run.Stderr
 	cmd.Stdin = nil
+<<<<<<< HEAD
 	// Set to an empty slice to get an empty environment. Nil means inherit.
 	cmd.Env = []string{}
 
 	log.Printf("Executing: %s", cmd.String())
 	run.Error = cmd.Run()
+=======
+
+	log.Printf("Executing: %s", cmd.String())
+	if err := cmd.Start(); err != nil {
+		return nil, status.Errorf(codes.Internal, "can't start %q %v", cmd.String(), err)
+	}
+
+	run.WaitError = cmd.Wait()
+>>>>>>> Move all command execution into a utility function.
 	run.ExitCode = cmd.ProcessState.ExitCode()
 	return run, nil
 }
