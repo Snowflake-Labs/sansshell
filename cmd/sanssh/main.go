0da8866ea7baabfdc3337ddfa106932309ba4f04
James Chacon <james.chacon@snowflake.com>
2021-11-02 10:33:50 -0700

    Add proxy flag and minor updates to use/not use it.

--- cmd/sanssh/main.go
+++ cmd/sanssh/main.go
@@ -15,6 +15,7 @@ import (
 	// Import the raw command clients you want, they automatically register
 	"github.com/Snowflake-Labs/sansshell/auth/mtls"
 	mtlsFlags "github.com/Snowflake-Labs/sansshell/auth/mtls/flags"
+	"github.com/Snowflake-Labs/sansshell/proxy/proxy"
 	_ "github.com/Snowflake-Labs/sansshell/services/ansible/client"
 	_ "github.com/Snowflake-Labs/sansshell/services/exec/client"
 	_ "github.com/Snowflake-Labs/sansshell/services/healthcheck/client"
@@ -26,13 +27,14 @@ import (
 var (
 	defaultAddress = "localhost:50042"
 	defaultTimeout = 3 * time.Second
+
+	address    = flag.String("address", defaultAddress, "Address to contact sansshell-server")
+	proxyAddr  = flag.String("proxy", "", "Address to contact for proxy to sansshell-server. If blank a direct connection to --address will be made")
+	timeout    = flag.Duration("timeout", defaultTimeout, "How long to wait for the command to complete")
+	credSource = flag.String("credential-source", mtlsFlags.Name(), fmt.Sprintf("Method used to obtain mTLS credentials (one of [%s])", strings.Join(mtls.Loaders(), ",")))
 )
 
 func main() {
-	address := flag.String("address", defaultAddress, "Address to contact sansshell-server")
-	timeout := flag.Duration("timeout", defaultTimeout, "How long to wait for the command to complete")
-	credSource := flag.String("credential-source", mtlsFlags.Name(), fmt.Sprintf("Method used to obtain mTLS credentials (one of [%s])", strings.Join(mtls.Loaders(), ",")))
-
 	subcommands.ImportantFlag("address")
 	subcommands.ImportantFlag("credential-source")
 	subcommands.Register(subcommands.HelpCommand(), "")
@@ -50,12 +52,16 @@ func main() {
 	}
 
 	// Set up a connection to the sansshell-server.
-	conn, err := grpc.Dial(*address, grpc.WithTransportCredentials(creds))
+	conn, err := proxy.Dial(*proxyAddr, []string{*address}, grpc.WithTransportCredentials(creds))
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "Could not connect to %q: %v\n", *address, err)
 		os.Exit(1)
 	}
-	defer conn.Close()
+	if *proxyAddr == "" {
+		defer conn.(*grpc.ClientConn).Close()
+	} else {
+		defer conn.(*proxy.ProxyConn).Close()
+	}
 
 	ctx, cancel := context.WithTimeout(ctx, *timeout)
 	defer cancel()
