12e5f6bad434c136738e6e6015cbc10bfcf5adab
James Chacon <james.chacon@snowflake.com>
2021-11-03 10:08:38 -0700

    Implement a basic client skeleton for proxy code (#24)
    * Update rego policy so sum/stat have the same test file as read does.
    
    * Skeleton implementation of client proxy code.
    
    Doesn't do anything today except setup for later and pass through if proxy isn't set.
    
    * Add proxy flag and minor updates to use/not use it.
    
    * Add some doc comments
    
    * Fix clients to take the connection interface not the concrete connection object.
    
    Thought I'd gotten all of these already...
    
    * Mark the channels receive only.
    
    Implement Dial in terms of DialContext
    
    * Fix closer to typecast to an io.Closer as a test.

---
+++ proxy/proxy/proxy.go
@@ -0,0 +1,84 @@
+// Package proxy provides the client side API for working with a proxy server.
+package proxy
+
+import (
+	"context"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+	anypb "google.golang.org/protobuf/types/known/anypb"
+)
+
+type ProxyConn struct {
+	// The targets we're proxying for currently.
+	targets []string
+	// The RPC connection to the proxy.
+	cc *grpc.ClientConn
+}
+
+// Invoke implements grpc.ClientConnInterface
+func (p *ProxyConn) Invoke(ctx context.Context, method string, args interface{}, reply interface{}, opts ...grpc.CallOption) error {
+	return status.Error(codes.Unimplemented, "not implemented")
+}
+
+// NewStream implements grpc.ClientConnInterface
+func (p *ProxyConn) NewStream(ctx context.Context, desc *grpc.StreamDesc, method string, opts ...grpc.CallOption) (grpc.ClientStream, error) {
+	return nil, status.Error(codes.Unimplemented, "not implemented")
+}
+
+// Additional methods needed for 1:N and N:N below.
+type ProxyRet struct {
+	Target string
+	Ret    anypb.Any
+	Error  error
+}
+
+// InvokeOneMany is used in proto generated code to implemened unary OneMany methods doing 1:N calls to the proxy.
+func (p *ProxyConn) InvokeOneMany(ctx context.Context, method string, args interface{}, opts ...grpc.CallOption) (<-chan *ProxyRet, error) {
+	return nil, status.Error(codes.Unimplemented, "not implemented")
+}
+
+type ProxyMany struct {
+	Target string
+	Method string
+	Args   interface{}
+}
+
+// InvokeManyMany is used in proto generated code to implemened unary ManyMany methods doing N:N calls to the proxy.
+func (p *ProxyConn) InvokeManyMany(ctx context.Context, calls []ProxyMany, opts ...grpc.CallOption) (<-chan *ProxyRet, error) {
+	return nil, status.Error(codes.Unimplemented, "not implemented")
+}
+
+// Close tears down the ProxyConn and closes all connections to it.
+func (p *ProxyConn) Close() error {
+	return p.cc.Close()
+}
+
+// Dial will connect to the given proxy and setup to send RPCs to the listed targets.
+// If proxy is blank and there is only one target this will return a normal grpc connection object (*grpc.ClientConn).
+// Otherwise this will return a *ProxyConn setup to act with the proxy.
+func Dial(proxy string, targets []string, opts ...grpc.DialOption) (grpc.ClientConnInterface, error) {
+	return DialContext(context.Background(), proxy, targets, opts...)
+}
+
+// DialContext is the same as Dial except the context provided can be used to cancel or expire the pending connection.
+// By default dial operations are non-blocking. See grpc.Dial for a complete explanation.
+func DialContext(ctx context.Context, proxy string, targets []string, opts ...grpc.DialOption) (grpc.ClientConnInterface, error) {
+	if proxy == "" {
+		if len(targets) != 1 {
+			return nil, status.Error(codes.InvalidArgument, "no proxy specified but more than one target set")
+		}
+		return grpc.DialContext(ctx, targets[0], opts...)
+	}
+	conn, err := grpc.DialContext(ctx, proxy, opts...)
+	if err != nil {
+		return nil, err
+	}
+	ret := &ProxyConn{
+		cc: conn,
+	}
+	// Make our own copy of these.
+	ret.targets = append(ret.targets, targets...)
+	return ret, nil
+}
