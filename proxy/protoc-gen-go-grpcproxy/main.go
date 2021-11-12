3e6bb16d145ee96a281968e239219d27cd8a1bab
James Chacon <james.chacon@snowflake.com>
2021-11-12 11:39:53 -0800

    A protoc plugin which adds OneMany methods for each service.
    Only does unary at the moment.
    
    Does via embedding of the original and extending. This way users can
    create a FooProxyClient and it "just works" for original methods.

---
+++ proxy/protoc-gen-go-grpcproxy/main.go
@@ -0,0 +1,154 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+
+	"google.golang.org/protobuf/compiler/protogen"
+	"google.golang.org/protobuf/types/pluginpb"
+)
+
+func main() {
+	protogen.Options{}.Run(func(plugin *protogen.Plugin) error {
+		plugin.SupportedFeatures = uint64(pluginpb.CodeGeneratorResponse_FEATURE_PROTO3_OPTIONAL)
+		for _, file := range plugin.Files {
+			if !file.Generate {
+				continue
+			}
+			generate(plugin, file)
+		}
+		return nil
+	})
+}
+
+const (
+	contextPackage   = protogen.GoImportPath("context")
+	grpcPackage      = protogen.GoImportPath("google.golang.org/grpc")
+	grpcProxyPackage = protogen.GoImportPath("github.com/Snowflake-Labs/sansshell/proxy/proxy")
+)
+
+func generate(plugin *protogen.Plugin, file *protogen.File) {
+	if len(file.Services) == 0 {
+		return
+	}
+	fn := file.GeneratedFilenamePrefix + "_grpcproxy.pb.go"
+	g := plugin.NewGeneratedFile(fn, file.GoImportPath)
+	g.P("// Auto generated code by protoc-gen-go-grpcproxy")
+	g.P("// DO NOT EDIT")
+	g.P()
+	g.P("// Adds OneMany versions of RPC methods for use by proxy clients")
+	g.P()
+	g.P("package ", file.GoPackageName)
+	g.P()
+	g.P("import (")
+	g.P(`"errors"`)
+	g.P(`"fmt"`)
+	g.P()
+	g.P(`grpcproxy "github.com/Snowflake-Labs/sansshell/proxy/proxy"`)
+	g.P(")")
+	g.P()
+
+	for _, service := range file.Services {
+		// Since we're adding additional methods on top of those defined we can skip
+		// if the service has no methods.
+		if len(service.Methods) == 0 {
+			continue
+		}
+
+		// Need the original names plus our Proxy added ones.
+		interfaceName := service.GoName + "Client"
+		interfaceNameProxy := interfaceName + "Proxy"
+		// Need the internal name of the struct.
+		goName := strings.ToLower(interfaceName[:1]) + interfaceName[1:]
+		clientStruct := goName
+		clientStructProxy := clientStruct + "Proxy"
+
+		// Client interface
+		//
+		// Have to do this as an embed as we can't just extend the original
+		// without replicating the entire grpc plugin and our additions.
+		// So we create a new one ending in Proxy which embeds the original
+		// and adds our OneMany methods.
+		g.P("// ", interfaceNameProxy, " is the superset of ", interfaceName, " which additionally includes the OneMany proxy methods")
+		g.P("type ", interfaceNameProxy, " interface {")
+		g.P(interfaceName)
+		for _, method := range service.Methods {
+			methodSignature(false, "", g, method, false)
+		}
+		g.P("}")
+		g.P()
+		g.P("type ", clientStructProxy, " struct {")
+		g.P("*", clientStruct)
+		g.P("}")
+		g.P()
+		g.P("func New", interfaceNameProxy, "(cc ", g.QualifiedGoIdent(grpcPackage.Ident("ClientConnInterface")), ") ", interfaceNameProxy, " {")
+		g.P("return &", clientStructProxy, "{New", interfaceName, "(cc).(*", clientStruct, ")}")
+		g.P("}")
+		g.P()
+
+		for _, method := range service.Methods {
+			g.P("type ", method.GoName, "ManyResponse struct {")
+			g.P("Target string")
+			g.P("Resp *", g.QualifiedGoIdent(method.Output.GoIdent))
+			g.P("Error error")
+			g.P("}")
+			g.P()
+
+			methodSignature(true, clientStructProxy, g, method, true)
+			unary := !method.Desc.IsStreamingClient() && !method.Desc.IsStreamingServer()
+			if unary {
+				g.P("manyRet, err := c.cc.(*", g.QualifiedGoIdent(grpcProxyPackage.Ident("ProxyConn")), ").InvokeOneMany(ctx, \"/", service.Desc.FullName(), "/", method.Desc.Name(), "\", in, opts...)")
+				g.P("if err != nil {")
+				g.P("return nil, err")
+				g.P("}")
+				g.P("ret := make(chan *", method.GoName, "ManyResponse)")
+				g.P("// A goroutine to retrive untyped responses and convert them to typed ones")
+				g.P("go func() {")
+				g.P("var resp *grpcproxy.ProxyRet")
+				g.P("var typedResp *", method.GoName, "ManyResponse")
+				g.P("for {")
+				g.P("select {")
+				g.P("case resp = <-manyRet:")
+				g.P("typedResp.Target = resp.Target")
+				g.P("typedResp.Error = resp.Error")
+				g.P("if resp.Error == nil {")
+				g.P("if err := resp.Resp.UnmarshalTo(typedResp.Resp); err != nil {")
+				g.P(`typedResp.Error = fmt.Errorf("can't decode any response - %v. Original Error - %v", err, resp.Error)`)
+				g.P("}")
+				g.P("}")
+				g.P("ret <- typedResp")
+				g.P("default:")
+				g.P("// All done so we can shut down.")
+				g.P("return")
+				g.P("}")
+				g.P("}")
+				g.P("}()")
+				g.P()
+				g.P("return ret, nil")
+				g.P("}")
+			} else {
+				g.P(`return nil, errors.New("not implemented")`)
+				g.P("}")
+			}
+			g.P()
+		}
+	}
+}
+
+func methodSignature(genFunc bool, structName string, g *protogen.GeneratedFile, method *protogen.Method, addBrace bool) {
+	prefix := ""
+	if genFunc {
+		prefix = "func (c *" + structName + ") "
+	}
+	sig := fmt.Sprintf("%s %sOneMany(ctx %s, ", prefix, method.GoName, g.QualifiedGoIdent(contextPackage.Ident("Context")))
+	unary := !method.Desc.IsStreamingClient() && !method.Desc.IsStreamingServer()
+	serverOnly := !method.Desc.IsStreamingClient() && method.Desc.IsStreamingServer()
+	if unary || serverOnly {
+		sig = fmt.Sprintf("%s in *%s, ", sig, g.QualifiedGoIdent(method.Input.GoIdent))
+	}
+	brace := ""
+	if addBrace {
+		brace = " {"
+	}
+	g.P(sig, "opts ...", g.QualifiedGoIdent(grpcPackage.Ident("CallOption")), ") (chan *", method.GoName, "ManyResponse, error)", brace)
+}
