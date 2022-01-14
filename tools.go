7240e5f22beabe0ea35c129ebda844a85853ebbe
James Chacon <james.chacon@snowflake.com>
2022-01-14 13:18:56 -0800

    Regenerate proxy/testsdata and add missing services

--- tools.go
+++ tools.go
@@ -19,6 +19,7 @@ import (
 // targets makes it possible to regenerate all services
 // by executing `go generate` against this file.
 
+//go:generate go generate ./proxy/testdata
 //go:generate go generate ./services/ansible
 //go:generate go generate ./services/exec
 //go:generate go generate ./services/healthcheck
