493022cad2d2c4abf32670046993f365061691fe
James Chacon <james.chacon@snowflake.com>
2022-01-24 15:02:08 -0800

    Remove debugging

--- testing/integrate.sh
+++ testing/integrate.sh
@@ -342,9 +342,8 @@ check_status $? /dev/null build binaries
 echo
 echo "Running tests (with tsan)"
 echo
-go test -count=100 -race -timeout 5m -run ^TestUnary/proxy_N_targets$ github.com/Snowflake-Labs/sansshell/proxy/proxy -v >& ${LOGS}/test.log
+go test -count=100 -race -timeout 5m -v ./... >& ${LOGS}/test.log
 check_status $? ${LOGS}/test.log test
-exit 0
 
 echo
 echo "Checking coverage - logs in ${LOGS}/cover.log"
