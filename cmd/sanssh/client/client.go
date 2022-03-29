3290fec1c10c1fa3390c01fa9ca9c0ea04864fec
James Chacon <james.chacon@snowflake.com>
2022-03-29 15:33:48 -0700

    Fix check to be correct

--- cmd/sanssh/client/client.go
+++ cmd/sanssh/client/client.go
@@ -70,7 +70,7 @@ const (
 // As this is intended to be called from main() it doesn't return errors and will instead exit on any errors.
 func Run(ctx context.Context, rs RunState) {
 	// Bunch of flag sanity checking
-	if len(rs.Targets) == 0 && rs.Proxy != "" {
+	if len(rs.Targets) == 0 && rs.Proxy == "" {
 		fmt.Fprintln(os.Stderr, "Must set target if no proxy")
 		os.Exit(1)
 	}
