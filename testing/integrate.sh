400e3ef13d3963031c5d837545b1532d2c2afcdc
James Chacon <james.chacon@snowflake.com>
2022-05-10 18:47:42 +0000

    Exclude proto dir from testing

--- testing/integrate.sh
+++ testing/integrate.sh
@@ -411,6 +411,7 @@ for i in \
   github.com/Snowflake-Labs/sansshell/services/ansible/client \
   github.com/Snowflake-Labs/sansshell/services/exec \
   github.com/Snowflake-Labs/sansshell/services/exec/client \
+  github.com/Snowflake-Labs/sansshell/services/fdb \
   github.com/Snowflake-Labs/sansshell/services/healthcheck \
   github.com/Snowflake-Labs/sansshell/services/healthcheck/client \
   github.com/Snowflake-Labs/sansshell/services/localfile \
