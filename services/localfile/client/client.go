Snowflake-Labs/sansshell

commit 5b5f2046d983c28091ff5c6c42e92dd40a9d6402

Improve symlink handling. (#211)
Fixes https://github.com/Snowflake-Labs/sansshell/issues/210

I've made dealing with symlinks nicer through adding the following

1. Use lstat when listing files in directory
2. Use lstat by default for `file stat`, imitating the behavior of `stat`
3. Add a new readlink command so that we can read symbolic links
4. Add a new symlink command so that we can create symbolic links
5. Add -l and -d commands to `file ls` because my muscle memory keeps typing them

Some manual testing on macos:

```
% ./sanssh --targets=localhost file ls -l /tmp/repro
-rw-r--r--   -      501        0                0 Mon Apr  3 16:11:57 UTC 2023 /tmp/repro/a
Lrwxr-xr-x   -      501        0               12 Mon Apr  3 16:13:10 UTC 2023 /tmp/repro/b
Lrwxr-xr-x   -      501        0                6 Mon Apr  3 16:12:55 UTC 2023 /tmp/repro/c
-rw-r--r--   -      501        0                0 Mon Apr  3 16:11:57 UTC 2023 /tmp/repro/d
% ./sanssh --targets=localhost file readlink /tmp/repro/b
/tmp/repro/d
% ./sanssh --targets=localhost file readlink /tmp/repro/a
readlink client error: rpc error: code = FailedPrecondition desc = file is not a symlink
```

services/localfile/client/client.go
@@ -50,10 +50,12 @@ func (*fileCmd) GetSubpackage(f *flag.FlagSet) *subcommands.Commander {
 	c.Register(&immutableCmd{}, "")
 	c.Register(&lsCmd{}, "")
 	c.Register(&readCmd{}, "")
+	c.Register(&readlinkCmd{}, "")
 	c.Register(&renameCmd{}, "")
 	c.Register(&rmCmd{}, "")
 	c.Register(&rmdirCmd{}, "")
 	c.Register(&statCmd{}, "")
+	c.Register(&symlinkCmd{}, "")
 	c.Register(&sumCmd{}, "")
 	c.Register(&tailCmd{}, "")
 	return c
@@ -183,6 +185,104 @@ func readFile(ctx context.Context, state *util.ExecuteState, req *pb.ReadActionR
 	return exit
 }
 
+type readlinkCmd struct {
+}
+
+func (*readlinkCmd) Name() string { return "readlink" }
+func (*readlinkCmd) Synopsis() string {
+	return "Print the value of a symbolic link or canonical file name."
+}
+func (*readlinkCmd) Usage() string {
+	return `readlink <path>
+  Print the value of a symbolic link or canonical file named by <path>.
+`
+}
+
+func (*readlinkCmd) SetFlags(f *flag.FlagSet) {}
+
+func (p *readlinkCmd) Execute(ctx context.Context, f *flag.FlagSet, args ...interface{}) subcommands.ExitStatus {
+	state := args[0].(*util.ExecuteState)
+	if f.NArg() == 0 {
+		fmt.Fprintln(os.Stderr, "Please specify a filename to read.")
+		return subcommands.ExitUsageError
+	}
+
+	filename := f.Args()[0]
+	req := &pb.ReadlinkRequest{
+		Filename: filename,
+	}
+
+	client := pb.NewLocalFileClientProxy(state.Conn)
+	respChan, err := client.ReadlinkOneMany(ctx, req)
+	if err != nil {
+		// Emit this to every error file as it's not specific to a given target.
+		for _, e := range state.Err {
+			fmt.Fprintf(e, "All targets - readlink client error: %v\n", err)
+		}
+		return subcommands.ExitFailure
+	}
+
+	retCode := subcommands.ExitSuccess
+	for r := range respChan {
+		if r.Error != nil {
+			fmt.Fprintf(state.Err[r.Index], "readlink client error: %v\n", r.Error)
+			retCode = subcommands.ExitFailure
+		} else {
+			fmt.Fprintln(state.Out[r.Index], r.Resp.Linkvalue)
+		}
+	}
+	return retCode
+}
+
+type symlinkCmd struct {
+}
+
+func (*symlinkCmd) Name() string { return "readlink" }
+func (*symlinkCmd) Synopsis() string {
+	return "Create a symbolic link."
+}
+func (*symlinkCmd) Usage() string {
+	return `symlink <target> <linkname>
+  Create a symbolic link from target to linkname, similar to "ln -s <target> <linkname>".
+`
+}
+
+func (*symlinkCmd) SetFlags(f *flag.FlagSet) {}
+
+func (p *symlinkCmd) Execute(ctx context.Context, f *flag.FlagSet, args ...interface{}) subcommands.ExitStatus {
+	state := args[0].(*util.ExecuteState)
+	if f.NArg() != 2 {
+		fmt.Fprintln(os.Stderr, "Please specify target and linkname.")
+		return subcommands.ExitUsageError
+	}
+
+	target := f.Args()[0]
+	linkname := f.Args()[1]
+	req := &pb.SymlinkRequest{
+		Target:   target,
+		Linkname: linkname,
+	}
+
+	client := pb.NewLocalFileClientProxy(state.Conn)
+	respChan, err := client.SymlinkOneMany(ctx, req)
+	if err != nil {
+		// Emit this to every error file as it's not specific to a given target.
+		for _, e := range state.Err {
+			fmt.Fprintf(e, "All targets - symlink client error: %v\n", err)
+		}
+		return subcommands.ExitFailure
+	}
+
+	retCode := subcommands.ExitSuccess
+	for r := range respChan {
+		if r.Error != nil {
+			fmt.Fprintf(state.Err[r.Index], "symlink client error: %v\n", r.Error)
+			retCode = subcommands.ExitFailure
+		}
+	}
+	return retCode
+}
+
 type tailCmd struct {
 	offset int64
 }
@@ -220,7 +320,9 @@ func (p *tailCmd) Execute(ctx context.Context, f *flag.FlagSet, args ...interfac
 	return readFile(ctx, state, req)
 }
 
-type statCmd struct{}
+type statCmd struct {
+	followLinks bool
+}
 
 func (*statCmd) Name() string     { return "stat" }
 func (*statCmd) Synopsis() string { return "Stat file(s) to stdout." }
@@ -230,7 +332,10 @@ func (*statCmd) Usage() string {
   `
 }
 
-func (*statCmd) SetFlags(f *flag.FlagSet) {}
+func (s *statCmd) SetFlags(f *flag.FlagSet) {
+	f.BoolVar(&s.followLinks, "L", false, "Use stat instead of lstat (as stat -L)")
+	f.BoolVar(&s.followLinks, "follow-links", false, "Use stat instead of lstat (as stat -L)")
+}
 
 func (s *statCmd) Execute(ctx context.Context, f *flag.FlagSet, args ...interface{}) subcommands.ExitStatus {
 	state := args[0].(*util.ExecuteState)
@@ -257,7 +362,7 @@ func (s *statCmd) Execute(ctx context.Context, f *flag.FlagSet, args ...interfac
 	go func() {
 		e := subcommands.ExitSuccess
 		for _, filename := range f.Args() {
-			if err := stream.Send(&pb.StatRequest{Filename: filename}); err != nil {
+			if err := stream.Send(&pb.StatRequest{Filename: filename, FollowLinks: s.followLinks}); err != nil {
 				// Emit this to every error file as it's not specific to a given target.
 				for _, e := range state.Err {
 					fmt.Fprintf(e, "All targets - stat: send error: %v\n", err)
@@ -757,7 +862,9 @@ func (*lsCmd) Usage() string {
 }
 
 func (p *lsCmd) SetFlags(f *flag.FlagSet) {
+	f.BoolVar(&p.long, "l", false, "If true prints out as ls -l would have done")
 	f.BoolVar(&p.long, "long", false, "If true prints out as ls -l would have done")
+	f.BoolVar(&p.directory, "d", false, "If true prints out the entry if it was a directory and nothing else (as ls -d)")
 	f.BoolVar(&p.directory, "directory", false, "If true prints out the entry if it was a directory and nothing else (as ls -d)")
 }
 

services/localfile/localfile.proto
@@ -28,7 +28,7 @@ service LocalFile {
   // Read reads a file from the disk and returns it contents.
   rpc Read(ReadActionRequest) returns (stream ReadReply) {}
 
-  // Stat returns metadata about a filesytem path.
+  // Stat returns metadata about a filesystem path.
   rpc Stat(stream StatRequest) returns (stream StatReply) {}
 
   // Sum calculates a sum over the data in a single file.
@@ -59,6 +59,12 @@ service LocalFile {
   // OS restrictions may apply if the old and new names are outside of the same
   // directory.
   rpc Rename(RenameRequest) returns (google.protobuf.Empty) {}
+
+  // Readlink prints the value of a symbolic link
+  rpc Readlink(ReadlinkRequest) returns (ReadlinkReply) {}
+
+  // Symlink creates a symbolic link
+  rpc Symlink(SymlinkRequest) returns (google.protobuf.Empty) {}
 }
 
 // ReadActionRequest indicates the type of read we're performing.
@@ -96,6 +102,8 @@ message ReadReply { bytes contents = 1; }
 message StatRequest {
   // absolute path to the target file
   string filename = 1;
+  // If true, use stat instead of lstat so that symlinks are followed
+  bool follow_links = 2;
 }
 
 // StatReply contains stat information for a single file.
@@ -243,4 +251,19 @@ message RmdirRequest {
 message RenameRequest {
   string original_name = 1;
   string destination_name = 2;
+}
+
+message ReadlinkRequest {
+  // The fully qualified path to the file to readlink.
+  string filename = 1;
+}
+
+message ReadlinkReply {
+  // The resolved link.
+  string linkvalue = 1;
+}
+
+message SymlinkRequest {
+  string target = 1;
+  string linkname = 2;
 }

services/localfile/localfile_grpc.pb.go
@@ -1,7 +1,22 @@
+// Copyright (c) 2019 Snowflake Inc. All rights reserved.
+//
+//Licensed under the Apache License, Version 2.0 (the
+//"License"); you may not use this file except in compliance
+//with the License.  You may obtain a copy of the License at
+//
+//http://www.apache.org/licenses/LICENSE-2.0
+//
+//Unless required by applicable law or agreed to in writing,
+//software distributed under the License is distributed on an
+//"AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+//KIND, either express or implied.  See the License for the
+//specific language governing permissions and limitations
+//under the License.
+
 // Code generated by protoc-gen-go-grpc. DO NOT EDIT.
 // versions:
-// - protoc-gen-go-grpc v1.2.0
-// - protoc             v3.21.9
+// - protoc-gen-go-grpc v1.3.0
+// - protoc             v3.21.12
 // source: localfile.proto
 
 package localfile
@@ -19,13 +34,28 @@ import (
 // Requires gRPC-Go v1.32.0 or later.
 const _ = grpc.SupportPackageIsVersion7
 
+const (
+	LocalFile_Read_FullMethodName              = "/LocalFile.LocalFile/Read"
+	LocalFile_Stat_FullMethodName              = "/LocalFile.LocalFile/Stat"
+	LocalFile_Sum_FullMethodName               = "/LocalFile.LocalFile/Sum"
+	LocalFile_Write_FullMethodName             = "/LocalFile.LocalFile/Write"
+	LocalFile_Copy_FullMethodName              = "/LocalFile.LocalFile/Copy"
+	LocalFile_List_FullMethodName              = "/LocalFile.LocalFile/List"
+	LocalFile_SetFileAttributes_FullMethodName = "/LocalFile.LocalFile/SetFileAttributes"
+	LocalFile_Rm_FullMethodName                = "/LocalFile.LocalFile/Rm"
+	LocalFile_Rmdir_FullMethodName             = "/LocalFile.LocalFile/Rmdir"
+	LocalFile_Rename_FullMethodName            = "/LocalFile.LocalFile/Rename"
+	LocalFile_Readlink_FullMethodName          = "/LocalFile.LocalFile/Readlink"
+	LocalFile_Symlink_FullMethodName           = "/LocalFile.LocalFile/Symlink"
+)
+
 // LocalFileClient is the client API for LocalFile service.
 //
 // For semantics around ctx use and closing/ending streaming RPCs, please refer to https://pkg.go.dev/google.golang.org/grpc/?tab=doc#ClientConn.NewStream.
 type LocalFileClient interface {
 	// Read reads a file from the disk and returns it contents.
 	Read(ctx context.Context, in *ReadActionRequest, opts ...grpc.CallOption) (LocalFile_ReadClient, error)
-	// Stat returns metadata about a filesytem path.
+	// Stat returns metadata about a filesystem path.
 	Stat(ctx context.Context, opts ...grpc.CallOption) (LocalFile_StatClient, error)
 	// Sum calculates a sum over the data in a single file.
 	Sum(ctx context.Context, opts ...grpc.CallOption) (LocalFile_SumClient, error)
@@ -47,6 +77,10 @@ type LocalFileClient interface {
 	// OS restrictions may apply if the old and new names are outside of the same
 	// directory.
 	Rename(ctx context.Context, in *RenameRequest, opts ...grpc.CallOption) (*emptypb.Empty, error)
+	// Readlink prints the value of a symbolic link
+	Readlink(ctx context.Context, in *ReadlinkRequest, opts ...grpc.CallOption) (*ReadlinkReply, error)
+	// Symlink creates a symbolic link
+	Symlink(ctx context.Context, in *SymlinkRequest, opts ...grpc.CallOption) (*emptypb.Empty, error)
 }
 
 type localFileClient struct {
@@ -58,7 +92,7 @@ func NewLocalFileClient(cc grpc.ClientConnInterface) LocalFileClient {
 }
 
 func (c *localFileClient) Read(ctx context.Context, in *ReadActionRequest, opts ...grpc.CallOption) (LocalFile_ReadClient, error) {
-	stream, err := c.cc.NewStream(ctx, &LocalFile_ServiceDesc.Streams[0], "/LocalFile.LocalFile/Read", opts...)
+	stream, err := c.cc.NewStream(ctx, &LocalFile_ServiceDesc.Streams[0], LocalFile_Read_FullMethodName, opts...)
 	if err != nil {
 		return nil, err
 	}
@@ -90,7 +124,7 @@ func (x *localFileReadClient) Recv() (*ReadReply, error) {
 }
 
 func (c *localFileClient) Stat(ctx context.Context, opts ...grpc.CallOption) (LocalFile_StatClient, error) {
-	stream, err := c.cc.NewStream(ctx, &LocalFile_ServiceDesc.Streams[1], "/LocalFile.LocalFile/Stat", opts...)
+	stream, err := c.cc.NewStream(ctx, &LocalFile_ServiceDesc.Streams[1], LocalFile_Stat_FullMethodName, opts...)
 	if err != nil {
 		return nil, err
 	}
@@ -121,7 +155,7 @@ func (x *localFileStatClient) Recv() (*StatReply, error) {
 }
 
 func (c *localFileClient) Sum(ctx context.Context, opts ...grpc.CallOption) (LocalFile_SumClient, error) {
-	stream, err := c.cc.NewStream(ctx, &LocalFile_ServiceDesc.Streams[2], "/LocalFile.LocalFile/Sum", opts...)
+	stream, err := c.cc.NewStream(ctx, &LocalFile_ServiceDesc.Streams[2], LocalFile_Sum_FullMethodName, opts...)
 	if err != nil {
 		return nil, err
 	}
@@ -152,7 +186,7 @@ func (x *localFileSumClient) Recv() (*SumReply, error) {
 }
 
 func (c *localFileClient) Write(ctx context.Context, opts ...grpc.CallOption) (LocalFile_WriteClient, error) {
-	stream, err := c.cc.NewStream(ctx, &LocalFile_ServiceDesc.Streams[3], "/LocalFile.LocalFile/Write", opts...)
+	stream, err := c.cc.NewStream(ctx, &LocalFile_ServiceDesc.Streams[3], LocalFile_Write_FullMethodName, opts...)
 	if err != nil {
 		return nil, err
 	}
@@ -187,7 +221,7 @@ func (x *localFileWriteClient) CloseAndRecv() (*emptypb.Empty, error) {
 
 func (c *localFileClient) Copy(ctx context.Context, in *CopyRequest, opts ...grpc.CallOption) (*emptypb.Empty, error) {
 	out := new(emptypb.Empty)
-	err := c.cc.Invoke(ctx, "/LocalFile.LocalFile/Copy", in, out, opts...)
+	err := c.cc.Invoke(ctx, LocalFile_Copy_FullMethodName, in, out, opts...)
 	if err != nil {
 		return nil, err
 	}
@@ -195,7 +229,7 @@ func (c *localFileClient) Copy(ctx context.Context, in *CopyRequest, opts ...grp
 }
 
 func (c *localFileClient) List(ctx context.Context, in *ListRequest, opts ...grpc.CallOption) (LocalFile_ListClient, error) {
-	stream, err := c.cc.NewStream(ctx, &LocalFile_ServiceDesc.Streams[4], "/LocalFile.LocalFile/List", opts...)
+	stream, err := c.cc.NewStream(ctx, &LocalFile_ServiceDesc.Streams[4], LocalFile_List_FullMethodName, opts...)
 	if err != nil {
 		return nil, err
 	}
@@ -228,7 +262,7 @@ func (x *localFileListClient) Recv() (*ListReply, error) {
 
 func (c *localFileClient) SetFileAttributes(ctx context.Context, in *SetFileAttributesRequest, opts ...grpc.CallOption) (*emptypb.Empty, error) {
 	out := new(emptypb.Empty)
-	err := c.cc.Invoke(ctx, "/LocalFile.LocalFile/SetFileAttributes", in, out, opts...)
+	err := c.cc.Invoke(ctx, LocalFile_SetFileAttributes_FullMethodName, in, out, opts...)
 	if err != nil {
 		return nil, err
 	}
@@ -237,7 +271,7 @@ func (c *localFileClient) SetFileAttributes(ctx context.Context, in *SetFileAttr
 
 func (c *localFileClient) Rm(ctx context.Context, in *RmRequest, opts ...grpc.CallOption) (*emptypb.Empty, error) {
 	out := new(emptypb.Empty)
-	err := c.cc.Invoke(ctx, "/LocalFile.LocalFile/Rm", in, out, opts...)
+	err := c.cc.Invoke(ctx, LocalFile_Rm_FullMethodName, in, out, opts...)
 	if err != nil {
 		return nil, err
 	}
@@ -246,7 +280,7 @@ func (c *localFileClient) Rm(ctx context.Context, in *RmRequest, opts ...grpc.Ca
 
 func (c *localFileClient) Rmdir(ctx context.Context, in *RmdirRequest, opts ...grpc.CallOption) (*emptypb.Empty, error) {
 	out := new(emptypb.Empty)
-	err := c.cc.Invoke(ctx, "/LocalFile.LocalFile/Rmdir", in, out, opts...)
+	err := c.cc.Invoke(ctx, LocalFile_Rmdir_FullMethodName, in, out, opts...)
 	if err != nil {
 		return nil, err
 	}
@@ -255,7 +289,25 @@ func (c *localFileClient) Rmdir(ctx context.Context, in *RmdirRequest, opts ...g
 
 func (c *localFileClient) Rename(ctx context.Context, in *RenameRequest, opts ...grpc.CallOption) (*emptypb.Empty, error) {
 	out := new(emptypb.Empty)
-	err := c.cc.Invoke(ctx, "/LocalFile.LocalFile/Rename", in, out, opts...)
+	err := c.cc.Invoke(ctx, LocalFile_Rename_FullMethodName, in, out, opts...)
+	if err != nil {
+		return nil, err
+	}
+	return out, nil
+}
+
+func (c *localFileClient) Readlink(ctx context.Context, in *ReadlinkRequest, opts ...grpc.CallOption) (*ReadlinkReply, error) {
+	out := new(ReadlinkReply)
+	err := c.cc.Invoke(ctx, LocalFile_Readlink_FullMethodName, in, out, opts...)
+	if err != nil {
+		return nil, err
+	}
+	return out, nil
+}
+
+func (c *localFileClient) Symlink(ctx context.Context, in *SymlinkRequest, opts ...grpc.CallOption) (*emptypb.Empty, error) {
+	out := new(emptypb.Empty)
+	err := c.cc.Invoke(ctx, LocalFile_Symlink_FullMethodName, in, out, opts...)
 	if err != nil {
 		return nil, err
 	}
@@ -268,7 +320,7 @@ func (c *localFileClient) Rename(ctx context.Context, in *RenameRequest, opts ..
 type LocalFileServer interface {
 	// Read reads a file from the disk and returns it contents.
 	Read(*ReadActionRequest, LocalFile_ReadServer) error
-	// Stat returns metadata about a filesytem path.
+	// Stat returns metadata about a filesystem path.
 	Stat(LocalFile_StatServer) error
 	// Sum calculates a sum over the data in a single file.
 	Sum(LocalFile_SumServer) error
@@ -290,6 +342,10 @@ type LocalFileServer interface {
 	// OS restrictions may apply if the old and new names are outside of the same
 	// directory.
 	Rename(context.Context, *RenameRequest) (*emptypb.Empty, error)
+	// Readlink prints the value of a symbolic link
+	Readlink(context.Context, *ReadlinkRequest) (*ReadlinkReply, error)
+	// Symlink creates a symbolic link
+	Symlink(context.Context, *SymlinkRequest) (*emptypb.Empty, error)
 }
 
 // UnimplementedLocalFileServer should be embedded to have forward compatible implementations.
@@ -325,6 +381,12 @@ func (UnimplementedLocalFileServer) Rmdir(context.Context, *RmdirRequest) (*empt
 }
 func (UnimplementedLocalFileServer) Rename(context.Context, *RenameRequest) (*emptypb.Empty, error) {
 	return nil, status.Errorf(codes.Unimplemented, "method Rename not implemented")
+}
+func (UnimplementedLocalFileServer) Readlink(context.Context, *ReadlinkRequest) (*ReadlinkReply, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method Readlink not implemented")
+}
+func (UnimplementedLocalFileServer) Symlink(context.Context, *SymlinkRequest) (*emptypb.Empty, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method Symlink not implemented")
 }
 
 // UnsafeLocalFileServer may be embedded to opt out of forward compatibility for this service.
@@ -447,7 +509,7 @@ func _LocalFile_Copy_Handler(srv interface{}, ctx context.Context, dec func(inte
 	}
 	info := &grpc.UnaryServerInfo{
 		Server:     srv,
-		FullMethod: "/LocalFile.LocalFile/Copy",
+		FullMethod: LocalFile_Copy_FullMethodName,
 	}
 	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
 		return srv.(LocalFileServer).Copy(ctx, req.(*CopyRequest))
@@ -486,7 +548,7 @@ func _LocalFile_SetFileAttributes_Handler(srv interface{}, ctx context.Context, 
 	}
 	info := &grpc.UnaryServerInfo{
 		Server:     srv,
-		FullMethod: "/LocalFile.LocalFile/SetFileAttributes",
+		FullMethod: LocalFile_SetFileAttributes_FullMethodName,
 	}
 	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
 		return srv.(LocalFileServer).SetFileAttributes(ctx, req.(*SetFileAttributesRequest))
@@ -504,7 +566,7 @@ func _LocalFile_Rm_Handler(srv interface{}, ctx context.Context, dec func(interf
 	}
 	info := &grpc.UnaryServerInfo{
 		Server:     srv,
-		FullMethod: "/LocalFile.LocalFile/Rm",
+		FullMethod: LocalFile_Rm_FullMethodName,
 	}
 	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
 		return srv.(LocalFileServer).Rm(ctx, req.(*RmRequest))
@@ -522,7 +584,7 @@ func _LocalFile_Rmdir_Handler(srv interface{}, ctx context.Context, dec func(int
 	}
 	info := &grpc.UnaryServerInfo{
 		Server:     srv,
-		FullMethod: "/LocalFile.LocalFile/Rmdir",
+		FullMethod: LocalFile_Rmdir_FullMethodName,
 	}
 	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
 		return srv.(LocalFileServer).Rmdir(ctx, req.(*RmdirRequest))
@@ -540,10 +602,46 @@ func _LocalFile_Rename_Handler(srv interface{}, ctx context.Context, dec func(in
 	}
 	info := &grpc.UnaryServerInfo{
 		Server:     srv,
-		FullMethod: "/LocalFile.LocalFile/Rename",
+		FullMethod: LocalFile_Rename_FullMethodName,
 	}
 	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
 		return srv.(LocalFileServer).Rename(ctx, req.(*RenameRequest))
+	}
+	return interceptor(ctx, in, info, handler)
+}
+
+func _LocalFile_Readlink_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+	in := new(ReadlinkRequest)
+	if err := dec(in); err != nil {
+		return nil, err
+	}
+	if interceptor == nil {
+		return srv.(LocalFileServer).Readlink(ctx, in)
+	}
+	info := &grpc.UnaryServerInfo{
+		Server:     srv,
+		FullMethod: LocalFile_Readlink_FullMethodName,
+	}
+	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
+		return srv.(LocalFileServer).Readlink(ctx, req.(*ReadlinkRequest))
+	}
+	return interceptor(ctx, in, info, handler)
+}
+
+func _LocalFile_Symlink_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+	in := new(SymlinkRequest)
+	if err := dec(in); err != nil {
+		return nil, err
+	}
+	if interceptor == nil {
+		return srv.(LocalFileServer).Symlink(ctx, in)
+	}
+	info := &grpc.UnaryServerInfo{
+		Server:     srv,
+		FullMethod: LocalFile_Symlink_FullMethodName,
+	}
+	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
+		return srv.(LocalFileServer).Symlink(ctx, req.(*SymlinkRequest))
 	}
 	return interceptor(ctx, in, info, handler)
 }
@@ -574,6 +672,14 @@ var LocalFile_ServiceDesc = grpc.ServiceDesc{
 		{
 			MethodName: "Rename",
 			Handler:    _LocalFile_Rename_Handler,
+		},
+		{
+			MethodName: "Readlink",
+			Handler:    _LocalFile_Readlink_Handler,
+		},
+		{
+			MethodName: "Symlink",
+			Handler:    _LocalFile_Symlink_Handler,
 		},
 	},
 	Streams: []grpc.StreamDesc{

services/localfile/localfile_grpcproxy.pb.go
@@ -30,6 +30,8 @@ type LocalFileClientProxy interface {
 	RmOneMany(ctx context.Context, in *RmRequest, opts ...grpc.CallOption) (<-chan *RmManyResponse, error)
 	RmdirOneMany(ctx context.Context, in *RmdirRequest, opts ...grpc.CallOption) (<-chan *RmdirManyResponse, error)
 	RenameOneMany(ctx context.Context, in *RenameRequest, opts ...grpc.CallOption) (<-chan *RenameManyResponse, error)
+	ReadlinkOneMany(ctx context.Context, in *ReadlinkRequest, opts ...grpc.CallOption) (<-chan *ReadlinkManyResponse, error)
+	SymlinkOneMany(ctx context.Context, in *SymlinkRequest, opts ...grpc.CallOption) (<-chan *SymlinkManyResponse, error)
 }
 
 // Embed the original client inside of this so we get the other generated methods automatically.
@@ -812,6 +814,140 @@ func (c *localFileClientProxy) RenameOneMany(ctx context.Context, in *RenameRequ
 	go func() {
 		for {
 			typedResp := &RenameManyResponse{
+				Resp: &emptypb.Empty{},
+			}
+
+			resp, ok := <-manyRet
+			if !ok {
+				// All done so we can shut down.
+				close(ret)
+				return
+			}
+			typedResp.Target = resp.Target
+			typedResp.Index = resp.Index
+			typedResp.Error = resp.Error
+			if resp.Error == nil {
+				if err := resp.Resp.UnmarshalTo(typedResp.Resp); err != nil {
+					typedResp.Error = fmt.Errorf("can't decode any response - %v. Original Error - %v", err, resp.Error)
+				}
+			}
+			ret <- typedResp
+		}
+	}()
+
+	return ret, nil
+}
+
+// ReadlinkManyResponse encapsulates a proxy data packet.
+// It includes the target, index, response and possible error returned.
+type ReadlinkManyResponse struct {
+	Target string
+	// As targets can be duplicated this is the index into the slice passed to proxy.Conn.
+	Index int
+	Resp  *ReadlinkReply
+	Error error
+}
+
+// ReadlinkOneMany provides the same API as Readlink but sends the same request to N destinations at once.
+// N can be a single destination.
+//
+// NOTE: The returned channel must be read until it closes in order to avoid leaking goroutines.
+func (c *localFileClientProxy) ReadlinkOneMany(ctx context.Context, in *ReadlinkRequest, opts ...grpc.CallOption) (<-chan *ReadlinkManyResponse, error) {
+	conn := c.cc.(*proxy.Conn)
+	ret := make(chan *ReadlinkManyResponse)
+	// If this is a single case we can just use Invoke and marshal it onto the channel once and be done.
+	if len(conn.Targets) == 1 {
+		go func() {
+			out := &ReadlinkManyResponse{
+				Target: conn.Targets[0],
+				Index:  0,
+				Resp:   &ReadlinkReply{},
+			}
+			err := conn.Invoke(ctx, "/LocalFile.LocalFile/Readlink", in, out.Resp, opts...)
+			if err != nil {
+				out.Error = err
+			}
+			// Send and close.
+			ret <- out
+			close(ret)
+		}()
+		return ret, nil
+	}
+	manyRet, err := conn.InvokeOneMany(ctx, "/LocalFile.LocalFile/Readlink", in, opts...)
+	if err != nil {
+		return nil, err
+	}
+	// A goroutine to retrive untyped responses and convert them to typed ones.
+	go func() {
+		for {
+			typedResp := &ReadlinkManyResponse{
+				Resp: &ReadlinkReply{},
+			}
+
+			resp, ok := <-manyRet
+			if !ok {
+				// All done so we can shut down.
+				close(ret)
+				return
+			}
+			typedResp.Target = resp.Target
+			typedResp.Index = resp.Index
+			typedResp.Error = resp.Error
+			if resp.Error == nil {
+				if err := resp.Resp.UnmarshalTo(typedResp.Resp); err != nil {
+					typedResp.Error = fmt.Errorf("can't decode any response - %v. Original Error - %v", err, resp.Error)
+				}
+			}
+			ret <- typedResp
+		}
+	}()
+
+	return ret, nil
+}
+
+// SymlinkManyResponse encapsulates a proxy data packet.
+// It includes the target, index, response and possible error returned.
+type SymlinkManyResponse struct {
+	Target string
+	// As targets can be duplicated this is the index into the slice passed to proxy.Conn.
+	Index int
+	Resp  *emptypb.Empty
+	Error error
+}
+
+// SymlinkOneMany provides the same API as Symlink but sends the same request to N destinations at once.
+// N can be a single destination.
+//
+// NOTE: The returned channel must be read until it closes in order to avoid leaking goroutines.
+func (c *localFileClientProxy) SymlinkOneMany(ctx context.Context, in *SymlinkRequest, opts ...grpc.CallOption) (<-chan *SymlinkManyResponse, error) {
+	conn := c.cc.(*proxy.Conn)
+	ret := make(chan *SymlinkManyResponse)
+	// If this is a single case we can just use Invoke and marshal it onto the channel once and be done.
+	if len(conn.Targets) == 1 {
+		go func() {
+			out := &SymlinkManyResponse{
+				Target: conn.Targets[0],
+				Index:  0,
+				Resp:   &emptypb.Empty{},
+			}
+			err := conn.Invoke(ctx, "/LocalFile.LocalFile/Symlink", in, out.Resp, opts...)
+			if err != nil {
+				out.Error = err
+			}
+			// Send and close.
+			ret <- out
+			close(ret)
+		}()
+		return ret, nil
+	}
+	manyRet, err := conn.InvokeOneMany(ctx, "/LocalFile.LocalFile/Symlink", in, opts...)
+	if err != nil {
+		return nil, err
+	}
+	// A goroutine to retrive untyped responses and convert them to typed ones.
+	go func() {
+		for {
+			typedResp := &SymlinkManyResponse{
 				Resp: &emptypb.Empty{},
 			}
 

commit 51b109348df1a5449f4715da1cbfd6d5b0b86492

Fix symlink subcommand name
This fixes a mistake I introduced in the cli in https://github.com/Snowflake-Labs/sansshell/pull/211. I had accidentally used the same subcommand name for both the `readlink` and `symlink` calls

services/localfile/client/client.go
@@ -237,7 +237,7 @@ func (p *readlinkCmd) Execute(ctx context.Context, f *flag.FlagSet, args ...inte
 type symlinkCmd struct {
 }
 
-func (*symlinkCmd) Name() string { return "readlink" }
+func (*symlinkCmd) Name() string { return "symlink" }
 func (*symlinkCmd) Synopsis() string {
 	return "Create a symbolic link."
 }