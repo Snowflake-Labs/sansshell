cf5b178f071cbe424d795e35e44b20e7572fccaa
Sergey Sudakovich <sergey.sudakovich@snowflake.com>
2023-06-12 12:43:23 -0700

    doc: live in a current year

--- services/power/server/reboot_linux_test.go
+++ services/power/server/reboot_linux_test.go
@@ -1,7 +1,7 @@
 //go:build linux
 // +build linux
 
-/* Copyright (c) 2019 Snowflake Inc. All rights reserved.
+/* Copyright (c) 2023 Snowflake Inc. All rights reserved.
 
    Licensed under the Apache License, Version 2.0 (the
    "License"); you may not use this file except in compliance
