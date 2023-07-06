885508bd2c45a36a1f4989eb80f024e614ac4b4b
Edbert Linardi <edbert.linardi@snowflake.com>
2023-07-06 14:13:54 -0700

    Add helper function to compare Options (#268)
    * add OptionsEqual
    
    * allow unexported and add unit test
    
    * add OptionSlicesEqual
    
    * remove comment about unexported struct

--- services/util/util.go
+++ services/util/util.go
@@ -30,6 +30,7 @@ import (
 	"syscall"
 
 	"github.com/go-logr/logr"
+	"github.com/google/go-cmp/cmp"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
 
@@ -78,6 +79,31 @@ type optionfunc func(*cmdOptions)
 
 func (f optionfunc) apply(opts *cmdOptions) {
 	f(opts)
+}
+
+// OptionsEqual returns true if the results of applying both Options are equal
+func OptionsEqual(a, b Option) bool {
+	aCmdOptions := &cmdOptions{}
+	bCmdOptions := &cmdOptions{}
+	a.apply(aCmdOptions)
+	b.apply(bCmdOptions)
+
+	return cmp.Equal(aCmdOptions, bCmdOptions, cmp.AllowUnexported(cmdOptions{}))
+}
+
+// OptionsEqual returns true if the results of applying all elements of both Option slices are equal
+func OptionSlicesEqual(a, b []Option) bool {
+	aCmdOptions := &cmdOptions{}
+	bCmdOptions := &cmdOptions{}
+
+	for _, opt := range a {
+		opt.apply(aCmdOptions)
+	}
+	for _, opt := range b {
+		opt.apply(bCmdOptions)
+	}
+
+	return cmp.Equal(aCmdOptions, bCmdOptions, cmp.AllowUnexported(cmdOptions{}))
 }
 
 // FailOnStderr is an option where the command will return an error if any output appears on stderr
