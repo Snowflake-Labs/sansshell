0f8242b3e39029d2f2972ca6b830d7b58341615e
Edbert Linardi <edbert.linardi@snowflake.com>
2023-08-28 13:15:48 -0700

    add ExecRemoteCommandMany

--- services/exec/client/utils.go
+++ services/exec/client/utils.go
@@ -3,6 +3,7 @@ package client
 import (
 	"context"
 	"errors"
+	"fmt"
 
 	"github.com/Snowflake-Labs/sansshell/proxy/proxy"
 	pb "github.com/Snowflake-Labs/sansshell/services/exec"
@@ -15,9 +16,40 @@ func ExecRemoteCommand(ctx context.Context, conn *proxy.Conn, binary string, arg
 		return nil, errors.New("ExecRemoteCommand only supports single targets")
 	}
 
-	c := pb.NewExecClient(conn)
-	return c.Run(ctx, &pb.ExecRequest{
+	result, err := ExecRemoteCommandMany(ctx, conn, binary, args...)
+	if err != nil {
+		return nil, err
+	}
+	if len(result) < 1 {
+		return nil, fmt.Errorf("ExecRemoteCommand error: received empty response")
+	}
+	return result[0].ExecResponse, nil
+}
+
+type ExecResponse struct {
+	*pb.ExecResponse
+	Index  int
+	Target string
+}
+
+func ExecRemoteCommandMany(ctx context.Context, conn *proxy.Conn, binary string, args ...string) ([]ExecResponse, error) {
+	c := pb.NewExecClientProxy(conn)
+	req := &pb.ExecRequest{
 		Command: binary,
 		Args:    args,
-	})
+	}
+	respChan, err := c.RunOneMany(ctx, req)
+	if err != nil {
+		return nil, err
+	}
+	result := make([]ExecResponse, len(conn.Targets))
+	for r := range respChan {
+		result[r.Index] = ExecResponse{
+			r.Resp,
+			r.Index,
+			r.Target,
+		}
+	}
+
+	return result, nil
 }
