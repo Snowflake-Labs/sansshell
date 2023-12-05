4240e187aa3759308aa686759d09f9d54b79b878
Calvin Chan <c.chan@snowflake.com>
2023-12-05 00:59:43 -0800

    Undo changes for client and rewrite error handling

--- services/packages/client/utils.go
+++ services/packages/client/utils.go
@@ -3,16 +3,15 @@ package client
 import (
 	"context"
 	"fmt"
-	"strings"
 
 	"github.com/Snowflake-Labs/sansshell/proxy/proxy"
 	pb "github.com/Snowflake-Labs/sansshell/services/packages"
 )
 
-// InstallManyRemoteServices is a helper function for installing binaries on multiple remote targets.
-func InstallManyRemoteServices(ctx context.Context, conn *proxy.Conn, system pb.PackageSystem, PackageName string, PackageVersion string, RepoEnabled string, RepoDisabled string) error {
+// InstallRemoteServiceMany is a helper function for installing binaries on multiple remote targets.
+func InstallRemoteServiceMany(ctx context.Context, conn *proxy.Conn, system pb.PackageSystem, PackageName string, PackageVersion string, RepoEnabled string, RepoDisabled string) error {
 	c := pb.NewPackagesClientProxy(conn)
-	respChan, err := c.InstallOneMany(ctx, &pb.InstallRequest{
+	resp, err := c.InstallOneMany(ctx, &pb.InstallRequest{
 		PackageSystem: system,
 		Name:          PackageName,
 		Version:       PackageVersion,
@@ -24,22 +23,15 @@ func InstallManyRemoteServices(ctx context.Context, conn *proxy.Conn, system pb.
 		return fmt.Errorf("can't install package %s - %v", PackageName, err)
 	}
 
-	var errorList []error
-	for resp := range respChan {
-		if resp.Error != nil {
-			err := fmt.Errorf("can't install package %s on target %s: %w", PackageName, resp.Target, resp.Error)
-			errorList = append(errorList, err)
+	errMsg := ""
+	for r := range resp {
+		if r.Error != nil {
+			errMsg += fmt.Sprintf("can't install packages %s on target %s (%d): %v", PackageName, r.Target, r.Index, r.Error)
 		}
 	}
-
-	if len(errorList) == 0 {
-		return nil
+	if errMsg != "" {
+		return fmt.Errorf("InstallRemoteServiceMany failed: %s", errMsg)
 	}
 
-	var errorMessages []string
-	for _, err := range errorList {
-		errorMessages = append(errorMessages, err.Error())
-	}
-
-	return fmt.Errorf("%s", strings.Join(errorMessages, "\n"))
+	return nil
 }
