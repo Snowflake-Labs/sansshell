346fb9f3feb010fb18ff28de59d3ada419b5fcf5
Steven Rhodes <steven.rhodes@snowflake.com>
2024-07-23 10:09:51 -0700

    Tweak debug_redact behavior (#460)
    * Tweak debug_redact behavior
    
    In sansshell we use redact to avoid logging sensitive info primarily for avoiding leaking secrets, not to provide privacy. I've switched our redaction from `--REDACTED--` to `REDACTED-<sha256hash>` so that it's possible to figure out if a field matches a known value while not being possible to learn the value purely from reading the field. The empty value is left unredacted because it should never be secret. I've also added support for bytes in addition to strings.
    
    Now that we have a more generous redaction, I've added it to a couple more fields that seem like they might accidentally contain secrets.
    
    * Add comment and more tests

--- auth/opa/rpcauth/redact_test.go
+++ auth/opa/rpcauth/redact_test.go
@@ -59,15 +59,21 @@ func TestGetRedactedInput(t *testing.T) {
 		ListScalar: []string{"s1"},
 		ListMsg: []*testdata.MyNested{
 			&testdata.MyNested{
-				Fine:      "ok",
-				Sensitive: "358===",
+				Fine:                   "ok",
+				Sensitive:              "358===",
+				SensitiveBytes:         []byte("123==="),
+				OneofField:             &testdata.MyNested_OneofFine{OneofFine: "oneof"},
+				SensitiveRepeatedBytes: [][]byte{[]byte("123===")},
+				SensitiveInt:           1,
 			},
 		},
 		MapScalar: map[string]string{"key": "value"},
 		MapMsg: map[string]*testdata.MyNested{
 			"key2": &testdata.MyNested{
-				Fine:      "also ok",
-				Sensitive: "456----",
+				Fine:           "also ok",
+				Sensitive:      "456----",
+				SensitiveBytes: []byte("456==="),
+				OneofField:     &testdata.MyNested_OneofSensitive{OneofSensitive: "oneof"},
 			},
 		},
 	}
@@ -92,8 +98,8 @@ func TestGetRedactedInput(t *testing.T) {
 
 				req := resultMessage.(*httppb.HostHTTPRequest)
 
-				assert.Equal(t, "--REDACTED--", req.Request.Headers[0].Values[0]) // field with debug_redact should be redacted
-				assert.Equal(t, "key0", req.Request.Headers[0].Key)               // field without debug_redact should not be redacted
+				assert.Equal(t, "REDACTED-f47373215435fa7979debe2467a2ca7779e9cb1d11810bf7447f2f2155f13ee1", req.Request.Headers[0].Values[0]) // field with debug_redact should be redacted
+				assert.Equal(t, "key0", req.Request.Headers[0].Key)                                                                            // field without debug_redact should not be redacted
 			},
 			errFunc: func(t *testing.T, err error) {
 				assert.NoError(t, err)
@@ -115,8 +121,8 @@ func TestGetRedactedInput(t *testing.T) {
 				payloadMsg, _ := proxyReqPayload.UnmarshalNew()
 				httpReq := payloadMsg.(*httppb.HostHTTPRequest)
 
-				assert.Equal(t, "--REDACTED--", httpReq.Request.Headers[0].Values[0]) // field with debug_redact should be redacted
-				assert.Equal(t, "key0", httpReq.Request.Headers[0].Key)               // field without debug_redact should not be redacted
+				assert.Equal(t, "REDACTED-f47373215435fa7979debe2467a2ca7779e9cb1d11810bf7447f2f2155f13ee1", httpReq.Request.Headers[0].Values[0]) // field with debug_redact should be redacted
+				assert.Equal(t, "key0", httpReq.Request.Headers[0].Key)                                                                            // field without debug_redact should not be redacted
 			},
 			errFunc: func(t *testing.T, err error) {
 				assert.NoError(t, err)
@@ -135,8 +141,14 @@ func TestGetRedactedInput(t *testing.T) {
 
 				req := resultMessage.(*testdata.TestRequest)
 
-				assert.Equal(t, "--REDACTED--", req.ListMsg[0].Sensitive)
-				assert.Equal(t, "--REDACTED--", req.MapMsg["key2"].Sensitive)
+				assert.Equal(t, "REDACTED-0c905d0153711846579c42dcd3346669ba75c0df127023b0a243d1f7390c51c4", req.ListMsg[0].Sensitive)
+				assert.Equal(t, "REDACTED-4676a64752815e068c008b5068b5c7ed3ca169045cb49d98fd399aa907709afb", string(req.ListMsg[0].SensitiveBytes))
+				assert.Equal(t, "REDACTED-4676a64752815e068c008b5068b5c7ed3ca169045cb49d98fd399aa907709afb", string(req.ListMsg[0].SensitiveRepeatedBytes[0]))
+				assert.Equal(t, "oneof", req.ListMsg[0].GetOneofFine())
+				assert.Equal(t, int64(0), req.ListMsg[0].GetSensitiveInt())
+				assert.Equal(t, "REDACTED-08fe17894f2ac6df3c4530391eecc64a1cf84593f85f1f018d0aae7581d28d4e", req.MapMsg["key2"].Sensitive)
+				assert.Equal(t, "REDACTED-fd1a71e8a6933fdaa5cfe7944c8d7533a79288c0c99b32f12753991bdee5b906", string(req.MapMsg["key2"].SensitiveBytes))
+				assert.Equal(t, "REDACTED-d24dd488138ce5c370d9db173fc97fa876ac17f11ded4eeaeb4d6170afd22b2d", req.MapMsg["key2"].GetOneofSensitive())
 			},
 			errFunc: func(t *testing.T, err error) {
 				assert.NoError(t, err)
