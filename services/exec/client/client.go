345225ad0c10d163e09b9a8a03f4ef8968fd1e1a
Marcin Walas <marcin.walas@snowflake.com>
2024-11-22 10:17:43 +0100

    Add README.md

--- services/exec/client/client.go
+++ services/exec/client/client.go
@@ -76,7 +76,7 @@ type runCmd struct {
 func (*runCmd) Name() string     { return "run" }
 func (*runCmd) Synopsis() string { return "Run provided command and return a response." }
 func (*runCmd) Usage() string {
-	return `run [--stream] [--user user] <command> [<args>...]:
+	return `run [--stream] [--user=user] <command> [<args>...]:
   Run a command remotely and return the response
 
 	Note: This is not optimized for large output or long running commands.  If
